e646c86d38ec31b639c71b67326ad18901b13caf
Li Yong <btt365com@gmail.com>
2018-08-10 15:08:33 +0800

    Initial Commit

---
+++ bet365/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bet365
+//
+//  Created by bet001 on 2018/8/10.
+//  Copyright © 2018年 Bet365. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
